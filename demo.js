dc82c50961c432e17fc08989d0c7144054c6f887
Mark <mdrago@frodo.(none)>
2009-12-06 23:26:33 -0500

    add collision detection, discs, balls

--- demo.js
+++ demo.js
@@ -15,16 +15,16 @@ var game = Class.create({
     main: function() {
         this.arena1 = new arena(this.ctx, this.canvas.width,
                                 this.canvas.height);
-        for (var i = 0; i < 100; i++) {
-            var item1;
-            if (Math.random() < .5) {
-                item1 = new circle();
-            } else {
-                item1 = new square();
-            }
-            
-            this.arena1.add_item(item1);
-        }
+        this.arena1.set_has_gravity(false);
+
+        var disc1 = new disc();
+        var spin1 = Math.floor(Math.random() * 200) - 100;
+        disc1.set_spin(spin1);
+        this.arena1.add_item(disc1, 100, 100);
+        
+        var ball1 = new ball();
+        this.arena1.add_item(ball1, 300, 110);
+        ball1.velx = 5;
     
         setInterval("game1.draw()", 1000 / this.fps);
     },
@@ -33,6 +33,7 @@ var game = Class.create({
         this.arena1.motion();
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.arena1.draw();
+        this.arena1.detect_collisions();
     }
 });
 
@@ -44,6 +45,7 @@ var arena = Class.create({
         this.items = new Array();
         this.gravity_accel = 1.15;
         this.negligible_vely = .3;
+        this.has_gravity = 1;
     },
 
     add_item: function(myitem, x, y) {
@@ -76,6 +78,41 @@ var arena = Class.create({
                 this.rm_item(myitem);
             }
         });
+    },
+    
+    set_has_gravity: function(value) {
+        this.has_gravity = value;
+    },
+    
+    detect_collisions: function() {
+        for (var i = 0; i < this.items.length - 1; i++) {
+            var item1 = this.items[i];
+            var x1_left = item1.x;
+            var x1_right = item1.x + item1.width;
+            var y1_top = item1.y;
+            var y1_bot = item1.y + item1.height;
+            
+            for (var j = i + 1; j < this.items.length; j++) {
+                var item2 = this.items[j];
+                var x2_left = item2.x;
+                var x2_right = item2.x + item2.width;
+                var y2_top = item2.y;
+                var y2_bot = item2.y + item2.height;
+                
+                if (((x1_left >= x2_left && x1_left <= x2_right) ||
+                     (x1_right >= x2_left && x1_right <= x2_right) ||
+                     (x2_left >= x1_left && x2_left <= x1_right) ||
+                     (x2_right >= x1_left && x2_right <= x1_right)) &&
+                    ((y1_top >= y2_top && y1_top <= y2_bot) ||
+                     (y1_bot >= y2_top && y1_bot <= y2_bot) ||
+                     (y2_top >= y1_top && y2_top <= y1_bot) ||
+                     (y2_bot >= y1_top && y2_bot <= y1_bot))) {
+                    //item1.collide(item2);
+                    //item2.collide(item1);
+                    alert("collision");
+                }
+            }
+        }
     }
 });
 
@@ -83,12 +120,10 @@ var item = Class.create({
     initialize: function(width, height) {
         this.setwidth(width);
         this.setheight(height);
-        this.velx = .1;
-        this.vely = .1;
+        this.velx = 0;
+        this.vely = 0;
         this.arena = null;
-        this.red = Math.floor(Math.random() * 255);
-        this.blue = Math.floor(Math.random() * 255);
-        this.green = Math.floor(Math.random() * 255);
+        this.setcolor();
     },
 
     add_to_arena: function(arena, x, y) {
@@ -103,15 +138,22 @@ var item = Class.create({
     },
 
     motion: function() {
-        this.gravity();
+        if (this.arena.has_gravity)
+            this.gravity();
         
         this.y += this.vely;
         this.x += this.velx;
         
-        //randomly bounce off the bottom
+        //bounce off top
+        if (this.y <= 0) {
+            this.y = 0;
+            this.vely *= -1;
+        }
+        
+        //bounce off the bottom
         if (this.y >= this.arena.height - this.height) {
             this.y = this.arena.height - this.height;
-            this.bounce();
+            this.vely *= -1;
         }
         
         //bounce off right wall
@@ -138,10 +180,17 @@ var item = Class.create({
             this.vely = this.arena.negligible_vely;
     },
     
-    //set random +y velocity, random +/-x velocity
-    bounce: function() {
-        this.setvely(Math.random() * -75);
-        this.setvelx((Math.random() * 10) - 5);
+    setcolor: function(red, green, blue) {
+        if (red === undefined)
+            red = Math.floor(Math.random() * 255);
+        if (green === undefined)
+            green = Math.floor(Math.random() * 255);
+        if (blue === undefined)
+            blue = Math.floor(Math.random() * 255);
+
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
     },
 
     setwidth: function(width) {
@@ -173,7 +222,7 @@ var item = Class.create({
     },
     
     getrgb: function() {
-        return "rgb(" + this.red + "," + this.blue + "," + this.green + ")";
+        return "rgb(" + this.red + "," + this.green + "," + this.blue + ")";
     }
 });
 
@@ -213,3 +262,32 @@ var circle = Class.create(item, {
         ctx.fill();
     }
 });
+
+var ball = Class.create(circle, {
+    initialize: function($super) {
+        $super(10);
+    }
+});
+
+var disc = Class.create(circle, {
+    initialize: function($super) {
+        $super(30);
+    },
+    
+    set_spin: function(spinval) {
+        this.spin = spinval;
+        
+        var red = 150;
+        var green = 150;
+        var blue = 150;
+        
+        if (spinval > 0) {
+            red += 25 + Math.floor(this.spin / 2);
+        } else {
+            blue += 25 + Math.floor(this.spin / -2);
+        }
+
+        this.setcolor(red, green, blue);
+    }
+});
+
